94019951070ccef42ed4cab5da6f9cc25ceb591b
Adria Stephanie <adriastephanie@hotmail.com>
2021-04-11 16:17:52 -0300

    create page architecture stopwatch-timer

--- stopwatch-timer/src/App.js
+++ stopwatch-timer/src/App.js
@@ -1,13 +1,23 @@
+import React, { Component} from 'react';
+import cronometro from './assets/cronometro.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>Oi</div>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  render(){
+    return(
+      <div className='container'>
+        <img src={cronometro} className='img'/>
+        <a className='timer'>0.0</a>
+        <div className='areaBtn'>
+          <a className='btn'>START</a> <br />
+          <a className='btn'>STOP</a>
+        </div>
+      </div>
+
+    );
+  }
+
 }
 
 export default App;

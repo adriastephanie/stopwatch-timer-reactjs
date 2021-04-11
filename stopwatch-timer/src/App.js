f6d1768d00e70fa7bca111d266d5378ba06d222c
Adria Stephanie <adriastephanie@hotmail.com>
2021-04-11 17:13:20 -0300

    feature stopwatch-timer

--- stopwatch-timer/src/App.js
+++ stopwatch-timer/src/App.js
@@ -3,15 +3,59 @@ import cronometro from './assets/cronometro.png';
 import './App.css';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      numero: 0,
+      botao: 'Start'
+    };
+
+    this.timer = null;
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
+
+  }
+
+  start() {
+
+    let state = this.state; 
+
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+      state.botao = 'START';
+    }else{
+      this.timer = setInterval(() =>{
+        let state = this.state;
+        state.numero += 0.1;
+        this.setState(state);
+      },100 );
+      state.botao = 'PAUSE';
+    }
+    this.setState(state)
+  }
+
+  stop(){
+    if(this.timer !== null){
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+
+    let state = this.state;
+    state.numero = 0;
+    state.botao = 'START';
+    this.setState(state);
+
+  }
 
   render(){
     return(
       <div className='container'>
         <img src={cronometro} className='img'/>
-        <a className='timer'>0.0</a>
+        <a className='timer'>{this.state.numero.toFixed(1)}</a>
         <div className='areaBtn'>
-          <a className='btn'>START</a> <br />
-          <a className='btn'>STOP</a>
+          <a className='btn' onClick={this.start}>{this.state.botao} </a> <br />
+          <a className='btn' onClick={this.stop}>STOP</a>
         </div>
       </div>
 
